4bf5590382bbb5fac80ef820782f2ebe929bb812
Sayalishewale1998 <sayalishewale1998@gmail.com>
2022-05-24 15:36:35 +0530

    first commit

---
+++ ConsoleApp1/ConsoleApp1/Class2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Class2
+    {
+        public static void Main()
+        {
+            int[] ar = new int[5];
+            int[] ar1 = new int[] { 1, 2, 3, 4, 5 };
+            
+            int[] ar3 = new int[5];
+
+            for (int i=0; i< ar1.Length; i++)
+            {
+                Console.WriteLine(ar1[i]);
+            }
+            string name = "sayali";
+            for(int i=0; i < name.Length; i++)
+            {
+                Console.WriteLine(name[i]);
+            }
+        }
+    }
+}
---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Secondhigh
+    {
+        static void Main()
+        {
+            int Max1, Max2;
+            int[] arr = { 21, 20, 56, 52, 41, 23, 32, 54, 97, 85 };
+            Console.WriteLine("Array Elements are :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.WriteLine(arr[i] + " ");
+            }
+            Max1 = Max2 = arr[0];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > Max1)
+                {
+                    Max2 = Max1;
+                    Max1 = arr[i];
+                }
+                else if (arr[i] > Max2)
+                {
+                    Max2 = arr[i];
+                }
+
+            }
+            Console.WriteLine("second highest number :" + Max2);
+            Console.ReadLine();
+        }
+    }
+}
+
